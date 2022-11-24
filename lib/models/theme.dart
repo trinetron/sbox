49e4367f5f1a8176820384678650e9ab31bc07d8
trinetron <tryliry@gmail.com>
2022-11-24 17:08:31 +0300

    -adaptive_theme, +neumorphic_theme

--- lib/models/theme.dart
+++ lib/models/theme.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic_null_safety/flutter_neumorphic.dart';
 
-final kLightTheme = ThemeData.light().copyWith(
-    primaryColor: Colors.black, appBarTheme: AppBarTheme(color: Colors.green));
-
-final kDarkTheme = ThemeData.dark().copyWith(
-    primaryColor: Colors.white, appBarTheme: AppBarTheme(color: Colors.brown));
+class ColorsSHM {
+  //   lightTheme:
+  final baseColorL = const Color.fromARGB(255, 255, 255, 255);
+  final lightSourceL = LightSource.topLeft;
+  final accentColorL = const Color.fromARGB(255, 157, 148, 177);
+  final depthL = 10.0;
+  //   darkTheme:
+  final baseColorD = const Color.fromARGB(255, 62, 62, 62);
+  final lightSourceD = LightSource.topLeft;
+  final accentColorD = const Color.fromARGB(255, 184, 175, 175);
+  final depthD = 6.0;
+  final setThemeMode = ThemeMode.light;
+}
